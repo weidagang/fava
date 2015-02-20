8b493beb78bb15c8912daff8660d55763b527bd7
dagang <weidagang@gmail.com>
2015-02-20 09:09:47 -0800

    Adds comments for compose.

--- src/fava/Composing.java
+++ src/fava/Composing.java
@@ -3,11 +3,16 @@ package fava;
 import fava.Currying.F1;
 
 /**
- * Utilities to composing functions 
+ * Utilities for composing functions. 
  * 
  * @author Dagang Wei (weidagang@gmail.com)
  */
 public class Composing {
+  /** 
+   * Composes two functions into one function.
+   * 
+   * <p> compose :: (T -> U) -> (U -> R) 
+   */
   public static <T, U, R> F1<T, R> compose(final F1<T, U> fn1, final F1<U, R> fn2) {
     return new F1<T, R>() {
       @Override
@@ -17,6 +22,11 @@ public class Composing {
     };
   }
 
+  /**
+   * Composes three functions into one function.
+   * 
+   * <p> compose(f1, f2, f3) = compose(compose(f1, f2), f3)
+   */
   public static <T, U1, U2, R> F1<T, R> compose(final F1<T, U1> fn1, final F1<U1, U2> fn2, final F1<U2, R> fn3) {
     return compose(compose(fn1, fn2), fn3);
   }
